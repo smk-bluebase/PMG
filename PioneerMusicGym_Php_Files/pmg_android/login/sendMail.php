533739596358d9571bb730acb5cf4df1bc49941c
smk-bluebase <71862757+smk-bluebase@users.noreply.github.com>
2021-04-09 10:18:01 +0530

    Update sendMail.php

--- PioneerMusicGym_Php_Files/pmg_android/login/sendMail.php
+++ PioneerMusicGym_Php_Files/pmg_android/login/sendMail.php
@@ -12,11 +12,11 @@ function sendMail($email, $subject, $message){
    $mail->SMTPSecure = 'ssl';
    $mail->Pool = true;
    $mail->Mailer   = 'smtp';
-   $mail->Username = 'info@bluebase.in';
-   $mail->Password = 'Bsplinfo@#2020';
+   $mail->Username = '';
+   $mail->Password = '';
 
-   $mail->setFrom('info@bluebase.in', 'Pioneer Music Gym App');
-   $mail->addReplyTo('info@bluebase.in', 'Pioneer Music Gym App');
+   $mail->setFrom('', 'Pioneer Music Gym App');
+   $mail->addReplyTo('', 'Pioneer Music Gym App');
    $mail->addAddress($email);
 
    $mail->isHTML(true);	
